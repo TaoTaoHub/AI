b4292d3f4bf607751b76ecb53b4f6906b99384dd
TaoTaoHub <wt.china@outlook.com>
2017-09-14 20:55:29 +0800

    use

--- useModel.py
+++ useModel.py
@@ -1,9 +1,12 @@
 #使用训练好的model
 import word2vec
 
+#300维向量
 model = word2vec.load('Word2VecModel.bin')
 words = model.cosine('努力')
-print(model['努力'])
-print(words)
-for x in words[0]:
-    print(model.vocab[x])
+vec = model['网']
+print(vec.dot(vec))
+#print(model['努力'])
+#print(words)
+#for x in words[0]:
+    #print(model.vocab[x])
